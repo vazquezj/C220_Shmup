5fd4e7aec6491f5392ab1e20fe8b450da75bc746
JORGEVAZQUEZ\Jorge <vazquezj@indiana.edu>
2017-09-12 16:08:04 -0400

    9-12-17 Catch-up from Thursday Sept. 7.

--- Empty_2D_project/Main.gd
+++ Empty_2D_project/Main.gd
@@ -3,3 +3,9 @@ extends Node
 func _ready():
 	print("My game works!")
 	pass
+
+func _process(delta):
+	if Input.is_action_just_pressed("Shoot"):
+		var newLaser = $"Laser".duplicate()
+		newLaser.position = $"Player".position
+		self.add_child(newLaser)
