584fef38bd08f6d8163b03ec949df02120608801
JORGEVAZQUEZ\Jorge <vazquezj@indiana.edu>
2017-09-21 17:17:42 -0400

    9-21-17 Health for enemy started.

--- Empty_2D_project/Enemy.gd
+++ Empty_2D_project/Enemy.gd
@@ -7,11 +7,7 @@ func _process(delta):
 	self.position.y += Speed
 
 func _on_Enemy_area_entered( area ):
-	if has_node("../Laser"):
-		self.Health -= 1
-	if has_node("../Missile"):
-		self.Health -= 3
+	self.Health -= 1
 	if Health <= 0:
 		self.queue_free()
-		area.queue_free()
 		$"../ScoreLabel".increase_score()
--- Empty_2D_project/Player.gd
+++ Empty_2D_project/Player.gd
@@ -9,5 +9,4 @@ func _process(delta):
 
 func _on_Player_area_entered( area ):
 	self.queue_free()
-	area.queue_free()
 	$"../GameOver".visible = true
